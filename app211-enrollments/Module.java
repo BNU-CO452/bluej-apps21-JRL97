c72939e147a10680d652fb60ebf07f1b02e11f24
JRL97 <91621546+JRL97@users.noreply.github.com>
2021-10-08 16:33:58 +0100

    Half of test 2

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,69 @@
+
+/**
+ * A class that contains information regarding modules available across courses.
+ *
+ * @author Jessica Leach
+ * @version 8/10/21
+ */
+public class Module
+{
+    // instance variables - replace the example below with your own
+
+    private String code;
+    private String title;
+    public int credit; 
+    
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module(String code, String title)
+    {
+       // initialise instance variables
+        this.code = code;
+        this.title= title;
+        credit = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getCode()
+    {
+        return this.code;
+        
+    }
+    
+    public String getTitle()
+    {
+        return this.title;
+        
+    }
+    
+    public int getCredit()
+    {
+        return this.credit;
+        
+    }
+    
+    public void setCredit(int credit)
+    {
+        this.credit = credit;
+    }
+    
+      /**
+     * Print the details of the course, the list
+     * of students enrolled and the module
+     */
+    public void print()
+    {
+        //printHeading();
+        
+        System.out.println(" Module Code: " + code + ": " + title);
+        System.out.println();
+    }
+    }
+
